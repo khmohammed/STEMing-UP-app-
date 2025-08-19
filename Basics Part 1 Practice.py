4bc0846aab432d85244027ae3e3c81fa7b47e10e
khmohammed <110481555+khmohammed@users.noreply.github.com>
2025-08-19 10:20:28 -0400

    Add files via upload

---
+++ Basics Part 1 Practice.py
@@ -0,0 +1,39 @@
+#integers
+print(14)
+
+#floats
+print(14.9)
+
+#strings
+print("Hello World!")
+print('Hello World!') #can use single quotations or double quotations
+print("14")
+
+#boolean values
+print(True)
+print(False)
+
+#arithmetic practice
+print(14/2)
+
+#create variables
+number = 14/2
+print(number % 5)
+
+#Variables Exercise
+student1_mark = 85
+student2_mark = 50
+
+#using arithmetic operators
+print(student1_mark - student2_mark)
+print(student1_mark / student2_mark)
+print(student1_mark // student2_mark)
+
+#using relational operators
+print(student1_mark == student2_mark) #False
+print(student1_mark != student2_mark) #True
+print(student1_mark > student2_mark) #True
+print(student1_mark < student2_mark) #False
+
+
+
